9a5a6b88f6a543e381b2ea5c2400e933c1595155
KendalSumler <kendal.sumler@gmail.com>
2016-11-04 17:48:11 -0500

    Matrices, Vector Work Derivative

---
+++ Matrices/Main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <math.h>
+#include "Matrix.h"
+#include <cassert>
+
+int main()
+{
+	Vector2D A = Vector2D(2, 1);
+	Vector2D B = Vector2D(1, 2);
+	Vector2D C = A + B;
+	assert(A + B == Vector2D(3,3));
+};
